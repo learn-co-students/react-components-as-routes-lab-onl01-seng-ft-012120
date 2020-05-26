04cee2c43a0c00f6ad2e772764523510d625c594
dwinniford <dwinniford@gmail.com>
2020-05-26 09:59:03 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,16 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(function(movie) {
+          return (<div> 
+            <h3>{movie.title}</h3>
+          <p>{movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+          </div>)
+        })}
     </div>
   );
 };
